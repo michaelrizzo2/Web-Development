9e52f8e13bc0faec7b4d53abc9aaa4c37aff2bf5
michaelrizzo2 <m_rizzo@live.com>
2015-04-23 16:01:10 -0400

    Added Breakline before looping through array by index

--- PHP Code/arrays.php
+++ PHP Code/arrays.php
@@ -8,6 +8,7 @@
 		//This will show how to get the length of the array
 		$my_array=array(1,32,4,5);
 		echo count($my_array);
+		echo "<br>";
 		//This will show how to iterate through an index based array
 		$my_array=array(1,32,4,5);
 		for ($x=0;$x<count($my_array);$x++)
