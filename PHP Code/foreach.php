635963dfeac2b2bb0d30aa80c0f901649a870f7d
michaelrizzo2 <m_rizzo@live.com>
2015-04-23 10:29:54 -0400

    Foreach loop demonstration in php

---
+++ PHP Code/foreach.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>for each loop in PHP</title>
+</head>
+
+<body>
+	<?php 
+		$numbers=array(1,2,3,4,5);
+		foreach ($numbers as $number)
+		{
+			echo "number is $number<br>";
+		}
+	?>
+</body>
+
+</html>
