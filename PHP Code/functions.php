25fc3c2078dda80d0e1ba402077396094e90eb22
michaelrizzo2 <m_rizzo@live.com>
2015-04-23 11:37:32 -0400

    demonstration of Function with default arguements in php

--- PHP Code/functions.php
+++ PHP Code/functions.php
@@ -5,23 +5,27 @@
 
 <body>
 	<?php 
+		//Function with no parameter
 		function writemessage()
 		{
 			echo "Hello world!";
 		}
 
+		writemessage();
+		
+		//Function with one parameter, an array and a foreach loop
 		function familyName($name)
 		{
 			echo "Name is $name<br>";
 		}
 
-		writemessage();
 		$name_array=array("Jani","Hege","Stale","Kai Jim","Borge");
 		foreach ($name_array as $name_entry)
 		{
 			familyName($name_entry);
 		}
 
+		//Function with two parameters
 		function familyNameYear($name,$year)
 		{
 			echo "$name was born in $year<br>";
@@ -30,6 +34,17 @@
 		familyNameYear("Hege","1975");
 		familyNameYear("Stale","1978");
 		familyNameYear("Kai Jim","1983");
+
+		//Function with an default arguement
+		function setHeight($height=50)
+		{
+			echo "The height is $height<br>";
+		}
+		setHeight(350);
+		setHeight();//This will use the default value of 50
+		setHeight(135);
+		setHeight(80);
+
 	?>
 </body>
 
