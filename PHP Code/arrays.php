84880da6fcfd4498322a1f953c062586d74989ea
michaelrizzo2 <m_rizzo@live.com>
2015-04-23 12:43:49 -0400

    Demonstration of getting length of an array in php

---
+++ PHP Code/arrays.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Arrays in PHP</title>
+</head>
+
+<body>
+	<?php 
+		//This will show how to get the length of the array
+		$my_array=array(1,32,4,5);
+		echo count($my_array);
+	?>
+</body>
+
+</html>
