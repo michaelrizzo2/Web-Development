c9b30e9236947d9eb2ae81ec41ed6b2a7d0679e0
michaelrizzo2 <m_rizzo@live.com>
2015-04-23 13:15:23 -0400

    Demonstration of iterating through an array with an index based for loop in php

--- PHP Code/arrays.php
+++ PHP Code/arrays.php
@@ -8,6 +8,13 @@
 		//This will show how to get the length of the array
 		$my_array=array(1,32,4,5);
 		echo count($my_array);
+		//This will show how to iterate through an index based array
+		$my_array=array(1,32,4,5);
+		for ($x=0;$x<count($my_array);$x++)
+		{
+			echo "The array entry is $my_array[$x] <br>";
+		}
+
 	?>
 </body>
 
