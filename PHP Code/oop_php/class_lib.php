d353b079f9ae9d6cef384c594b0a622115656878
michaelrizzo2 <m_rizzo@live.com>
2015-04-24 08:21:44 -0400

    Step 4,5, and 6  add getter and setter methods, and  for instance variables to php class in class_lib.php

--- PHP Code/oop_php/class_lib.php
+++ PHP Code/oop_php/class_lib.php
@@ -8,6 +8,16 @@
 		class person
 		{
 			var $name;	
+
+			function set_name($new_name)
+			{
+				$this->name=$new_name;
+			}
+
+			function get_name()
+			{
+				return $this->name;	
+			}
 		}
 	?>
 </body>
