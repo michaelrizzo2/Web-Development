21a025c6a4beb4e9859ac86d0cf808a661760229
michaelrizzo2 <m_rizzo@live.com>
2015-04-24 14:42:52 -0400

    Step 11 accessing an objects data

--- PHP Code/oop_php/index.php
+++ PHP Code/oop_php/index.php
@@ -5,10 +5,12 @@
 
 <body>
 	<?php 
+
 		include("class_lib.php");
 		$michael=new person();
 		$michael->set_name("Michael Rizzo");
 		echo "Michaels full name is ". $michael->get_name();
+
 	?>
 </body>
 
