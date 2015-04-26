d78f7820bb925ae67b3e6991f2a8b65fffb6bab0
michaelrizzo2 <m_rizzo@live.com>
2015-04-26 12:49:23 -0400

    Step 13 creating a constructor to set name property for each object

--- PHP Code/oop_php/class_lib.php
+++ PHP Code/oop_php/class_lib.php
@@ -11,6 +11,11 @@
 		//Step 3 This will add a data variable to our person class
 		var $name;
 
+		//Step 13 setting up constructor
+		function __construct($new_name)
+		{
+			$this->name=$new_name;
+		}
 		//Step 4,5 and 6 getter and setter methods, and $this keyword for instance variables
 		function set_name($new_name)
 		{
