9d82cb07abab0994224c47872704cd4d89c8e96b
michaelrizzo2 <m_rizzo@live.com>
2015-04-26 12:36:36 -0400

    Step 10 setting up an objects properties

--- PHP Code/oop_php/index.php
+++ PHP Code/oop_php/index.php
@@ -9,6 +9,8 @@
 		include("class_lib.php");
 		//step 8,9 set up class and show use of new keyword
 		$michael=new person();
+		//Step 10 set an objects properties
+		$michael->set_name("Michael Rizzo");
 	?>
 </body>
 
