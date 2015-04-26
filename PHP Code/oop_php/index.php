07dd06a3520147b95237cb966702b86754e1e68f
michaelrizzo2 <m_rizzo@live.com>
2015-04-26 12:54:53 -0400

    Step 14 creating a object using a constructor

--- PHP Code/oop_php/index.php
+++ PHP Code/oop_php/index.php
@@ -8,9 +8,11 @@
 		//Step 7 include class code 
 		include("class_lib.php");
 		//step 8,9 set up class and show use of new keyword
-		$michael=new person();
+		//$michael=new person();
 		//Step 10 set an objects properties
-		$michael->set_name("Michael Rizzo");
+		//$michael->set_name("Michael Rizzo");
+		//Step 14 create object with constructor
+		$michael=new person("Michael Rizzo");
 		//Step 11 Accessing an objects Data
 		echo "Michael's full name is ". $michael->get_name();
 	?>
