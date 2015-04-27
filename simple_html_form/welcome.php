7dc5d6d84b4d677a6aea0a87e9e1f62c1ff5c139
michaelrizzo2 <m_rizzo@live.com>
2015-04-27 12:59:43 -0400

    added semicolon after echoing email address input in php

--- simple_html_form/welcome.php
+++ simple_html_form/welcome.php
@@ -1,6 +1,6 @@
 <html>
 	<body>
 		Welcome <?php echo $_POST["name"];?>
-		Your email address is:<?php echo $_POST["email"]?>
+		Your email address is:<?php echo $_POST["email"];?>
 	</body>
 </html>
