64aad6019a1ccc25303c6a87fb5f6eab4708c2e7
michaelrizzo2 <m_rizzo@live.com>
2018-02-09 23:35:06 -0500

    added logic to go to proper url

--- Node_app/server.js
+++ Node_app/server.js
@@ -1,9 +1,12 @@
 var http=require("http");
+var url=require("url");
 function start()
 {
 function onRequest(request,response)
 {
-    console.log("Request Received")
+    //adding logic to get proper url pathname
+    var pathname=url.parse(request.url).pathname;
+    console.log("Request for  "+pathname+"Received")
     response.writeHead(200,{"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
