ea6d5921d8ba968b209169256ab713f694a7ce94
michaelrizzo2 <m_rizzo@live.com>
2018-02-10 12:22:54 -0500

    added logic to chek for proper request handler

--- Node_app/router.js
+++ Node_app/router.js
@@ -1,6 +1,15 @@
 function route(pathname)
 {
     console.log("About to route a request for "+ pathname)
+    if(typeof handle[pathname]==='function')
+    {
+        handle[pathname]()
+    }
+
+    else
+    {
+        console.log("No Request handler found for" + pathname)
+    }
 }
 
 exports.route=route
