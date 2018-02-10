de282ad058beca6cb6cbf4141f27cd4e8cca0a85
michaelrizzo2 <m_rizzo@live.com>
2018-02-09 23:24:25 -0500

    Proved server has been started and request has been recieved

--- Node_app/server.js
+++ Node_app/server.js
@@ -2,9 +2,11 @@ var http=require("http");
 
 function onRequest(request,response)
 {
+    console.log("Request Received")
     response.writeHead(200,{"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+console.log("Server has Started")
