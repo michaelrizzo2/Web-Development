85596757eb094bda8c10a969276a9cd62e853751
michaelrizzo2 <m_rizzo@live.com>
2018-02-10 20:16:27 -0500

    changed route.route to route

--- Node_app/server.js
+++ Node_app/server.js
@@ -4,16 +4,16 @@ var http=require("http");
 var url=require("url");
 function start(route,handle)
 {
-function onRequest(request,response)
-{
-    //adding logic to get proper url pathname
-    var pathname=url.parse(request.url).pathname;
-    console.log("Request for  "+pathname+"Received")
-    route.route(handle,pathname)
-    response.writeHead(200,{"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}
+    function onRequest(request,response)
+    {
+        //adding logic to get proper url pathname
+        var pathname=url.parse(request.url).pathname;
+        console.log("Request for  "+pathname+" Received")
+        route(handle,pathname)
+        response.writeHead(200,{"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
 
 
 
