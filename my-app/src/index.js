a0532b01b2fb804a1e141eb8adf03ec61eeb2b9b
michaelrizzo2 <m_rizzo@live.com>
2018-02-11 22:01:54 -0500

    removed constructor from board class,moved handle click to game class.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,39 +9,13 @@ function  Square(props){
   )
 }
   class Board extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={squares:Array(9).fill(null),xIsNext:true};
-    }
-    handleClick(i)
-    {
-        const squares=this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i])
-        {
-          return;
-        }
-        squares[i]=this.state.xIsNext ? "X" :"O"
-        this.setState({squares:squares,xIsNext:!this.state.xIsNext})
-    }
     renderSquare(i) {
-    return (<Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>);
+    return (<Square value={this.props.squares[i]} onClick={()=>this.props.onClick(i)}/>);
     }
   
     render() {
-      const winner=calculateWinner(this.state.squares)
-      let status
-      if (winner)
-      {
-        status="Winner "+winner;
-      }
-
-      else
-      {          status="Next Player: "+(this.state.xIsNext? 'X' :'O')
-      }
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -63,6 +37,30 @@ function  Square(props){
   }
   
   class Game extends React.Component {
+    constructor(props)
+    {
+      super(props)
+      this.state={
+        history:[{squares:Array(9).fill(null)}],xIsNext:true
+      }
+    }
+    handleClick(i)
+    {
+      const history=this.state.history;
+      const current=history[history.length-1]
+      const squares=current.squares.slice()
+        if (calculateWinner(squares) || squares[i])
+        {
+          return;
+        }
+        squares[i]=this.state.xIsNext ? "X" :"O"
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          xIsNext: !this.state.xIsNext,
+        });
+    }
     render() {
       return (
         <div className="game">
