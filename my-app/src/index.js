eb07c059c96bb4c9fc17a9b18b61e0796e8e5832
michaelrizzo2 <m_rizzo@live.com>
2018-02-11 22:58:58 -0500

    Fully implemented board history.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -50,12 +50,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber:0
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,this.state.stepNumber+1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -66,13 +67,19 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber:history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
   
+  jumpTo(step)
+  {
+this.setState({stepNumber:step,xIsNext:step%2===0})
+  }
+  
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves=history.map((step,move)=>
     {
@@ -80,7 +87,7 @@ class Game extends React.Component {
 
       return (
 
-        <li><button onClick={() => this.jumpTo(move)}>{desc}</button></li>
+        <li key={move}><button onClick={() => this.jumpTo(move)}>{desc}</button></li>
       )
     })
     let status;
